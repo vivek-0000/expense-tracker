82ce92793fdcbdf7fe018245d8ed512e90b345dc
unknown <vivekthegreat0000@gmail.com>
2026-04-07 16:21:14 +0530

    fix db crash

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./expenses.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./data.db"
+
+connect_args = {"check_same_thread": False}
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
+    connect_args=connect_args
 )
 
 SessionLocal = sessionmaker(bind=engine)
